50790f435efce8fb08510b471bb8148381010cfc
Unknown <povilukui@gmail.com>
2019-02-26 16:49:44 -0500

    setting up tests

---
+++ new_odrivetool_tests.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import UI_mainwindow2
+
+import odrive
+from odrive.enums import *
+
+import json
+import glob
+import datetime
+
+import os
+
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
+
+from serialThread import odriveWorker
+
+ICON_TRUE = "Icons/True.jpg"
+ICON_FALSE = "Icons/False.jpg"
+ICON_NOSTATE = "Icons/NoState.jpg"
+
+class ExampleApp(QtWidgets.QMainWindow, UI_mainwindow2.Ui_MainWindow):
+
+	app_name = "Odrive Tester"
+	def __init__(self):
+		# Simple reason why we use it here is that it allows us to
+		# access variables, methods etc in the design.py file
+		super(self.__class__, self).__init__()
+		self.setupUi(self)  # This is defined in design.py file automatically
+							# It sets up layout and widgets that are defined
+
+def main():
+	app = QtWidgets.QApplication(sys.argv)  # A new instance of QApplication
+	form = ExampleApp()                 # We set the form to be our (design)
+	form.show()                         # Show the form
+	app.exec_()                         # and execute the app
+
+
+if __name__ == '__main__':              # if we're running file directly and not importing it
+	main()                              # run the main function
